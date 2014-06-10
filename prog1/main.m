c621323bb45912849ef252346e879ffbe1c20333
RobertRuttenberg <rdruttenberg@gmail.com>
2014-06-10 13:56:48 -0400

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Robert Ruttenberg on 6/10/14.
+//  Copyright (c) 2014 Robert Ruttenberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
